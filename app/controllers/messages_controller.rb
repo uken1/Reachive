725300f3bcde862826b2cc13ccb1263aa4f0c0a4
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-09-05 12:40:10 +0000

    DIVE19-1完成20170905_1

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,26 +1,40 @@
 class MessagesController < ApplicationController
-  
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
   end
-  
-def index
-  @messages = @conversation.messages
-  if @messages.length > 10
+
+  def index
+    @messages = @conversation.messages
+   if @messages.length > 10
     @over_ten = true
     @messages = @messages[-10..-1]
-  end
+   end
 
-  if params[:m]
+   if params[:m]
     @over_ten = false
     @messages = @conversation.messages
-  end
+   end
 
-  if @messages.last
+   if @messages.last
     if @messages.last.user_id != current_user.id
      @messages.last.read = true
     end
+   end
+
+    @message = @conversation.messages.build
+
   end
 
-  @message = @conversation.messages.build
+  def create
+    @message = @conversation.messages.build(message_params)
+    #binding.pry
+    if @message.save
+     redirect_to conversation_messages_path(@conversation)
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 end
