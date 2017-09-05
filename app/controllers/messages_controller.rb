542a22936ec593c2eb456398dcb3e06d857fbf3b
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-09-05 12:06:09 +0000

    DIVE19-1一通り完了。conversation_controllerに何も入力されていなかった。なぜかな？その後はOK.２０１７０８０５_4

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,4 @@
 class MessagesController < ApplicationController
-  
   before_action do
     @conversation = Conversation.find(params[:conversation_id])
   end
@@ -23,4 +22,19 @@ def index
   end
 
   @message = @conversation.messages.build
+  
+end
+
+  def create
+    @message = @conversation.messages.build(message_params)
+    #binding.pry
+    if @message.save
+     redirect_to conversation_messages_path(@conversation)
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 end
