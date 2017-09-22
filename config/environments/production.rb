dcdd40c2274ae5bcb4a12e29976ece82ffc04646
Kenichi Utsumi <k-utsumi@nttpc.co.jp>
2017-09-22 02:45:03 +0000

    Daikadai3_コメント入力が空白時のvalidationを追加_20170922_2_デプロイ失敗再トライ_origin masterが先

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,6 @@ password: ENV['SENDGRID_PASSWORD'],
  enable_starttls_auto: true
 }
 
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
